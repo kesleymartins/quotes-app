95e2981f5b8fb3e9037713fcb28fb46ae32f0c1e
kesleymartins <kesleymartins>
2023-04-13 12:15:50 -0300

    create quotes controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,50 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: %i[show edit update destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path, notice: 'Quote criado com sucesso!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: 'Quote atualizado com sucesso!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, notice: 'Quote removida com sucesso!'
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
+end
